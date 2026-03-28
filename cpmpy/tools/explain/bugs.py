da89dca3ccc8ec11a1380f53bfe50670997df357
Robin Halford <robin.halford@student.kuleuven.be>
2026-03-28 12:45:04 +0100

    file to test bugs

--- cpmpy/tools/explain/bugs.py
+++ cpmpy/tools/explain/bugs.py
@@ -5,27 +5,20 @@ import pickle as pkl
 
 def test_pkl_marco_bug():
 
-    filename= "BusScheduling-cnt-t1.xml.lzma_0.25.pkl"
+    filename= "kbtree-9-7-3-5-90-36.xml.lzma.pkl"
     path = "cpmpy/tools/explain/experiments_diverse/data/XCSP_MUS/" + filename
 
-    # model = cp.Model().from_file(path)
-    with open(path, 'rb') as f:
-        model = pkl.load(f)
+    model = cp.Model().from_file(path)
+    # with open(path, 'rb') as f:
+        # model = pkl.load(f)
 
     print("Printing constraints: \n\n")
     for c in model.constraints:
-        print(c + "\n\n")
+        print(c + "\n")
     
     print("Solving model: \n\n")
     result = model.solve("exact")
     print("Result: " + str(result))
-
-    """
-    generator = marco(model.constraints, solver="exact", map_solver="exact", return_mcs=False)
-    for kind, subset in generator:
-        print("Found a MUS \n")
-        break
-    """
 
 
 if __name__ == "__main__":
